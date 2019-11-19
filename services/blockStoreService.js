d04bb6bfeb5ac396c533d876a9f6fe801240838d
ComBba <cent@naver.com>
2019-11-20 08:32:28 +0900

    chaged web3 provider

--- services/blockStoreService.js
+++ services/blockStoreService.js
@@ -21,7 +21,10 @@ var blockstore = function (app) {
 		function (next) {
 			//console.log("[▷▷▷ Start ▷▷▷][blockStoreService]", printDateTime());
 			var web3 = new Web3();
-			web3.setProvider(new web3.providers.HttpProvider(configConstant.localRPCaddress));
+			var config = app.get('config');
+			if (!web3.currentProvider)
+				web3.setProvider(config.selectParity());
+
 			var data = {};
 			data.dbLastBlock = 0;
 			data.blockCount = 2000;
@@ -137,189 +140,189 @@ var blockstore = function (app) {
 
 									if (block.transactions && block.transactions.length > 0) {
 										async.waterfall([
-												function (incallback) {
-													web3.trace.filter({
-														"fromBlock": web3.toHex((new BigNumber(block.number)).toNumber()),
-														"toBlock": web3.toHex((new BigNumber(block.number)).toNumber())
-													}, function (err, traces) {
-														if (err) {
-															console.log("[ERROR][web3.trace.filter] ", block.number);
-															console.log(err);
+											function (incallback) {
+												web3.trace.filter({
+													"fromBlock": web3.toHex((new BigNumber(block.number)).toNumber()),
+													"toBlock": web3.toHex((new BigNumber(block.number)).toNumber())
+												}, function (err, traces) {
+													if (err) {
+														console.log("[ERROR][web3.trace.filter] ", block.number);
+														console.log(err);
+													}
+													//traces[key].calltype = "to"; //account.js에서 처리해야함
+													incallback(err, traces);
+												});
+											},
+											function (traces, incallback) {
+												async.eachSeries(traces, function (trace, ineachCallback) {
+													trace.isContract = false;
+													if (!trace.action) {
+														trace.action = {};
+													}
+													trace.action._value = '';
+													trace.action._to = '';
+													if (trace.type === 'reward') {
+														if (trace.action.value && web3.toHex((new BigNumber(trace.action.value)).toNumber()) == '0x4563918244f40000') {
+															var gasUsed = new BigNumber(block.gasUsed);
+															var totalGasUsed = new BigNumber(gasUsed.times(block.transactions[block.transactions.length - 1].gasPrice));
+															var actionValue = new BigNumber(trace.action.value);
+															trace.action.value = web3.toHex(actionValue.plus(totalGasUsed).toNumber());
 														}
-														//traces[key].calltype = "to"; //account.js에서 처리해야함
-														incallback(err, traces);
-													});
-												},
-												function (traces, incallback) {
-													async.eachSeries(traces, function (trace, ineachCallback) {
-														trace.isContract = false;
-														if (!trace.action) {
-															trace.action = {};
-														}
-														trace.action._value = '';
-														trace.action._to = '';
-														if (trace.type === 'reward') {
-															if (trace.action.value && web3.toHex((new BigNumber(trace.action.value)).toNumber()) == '0x4563918244f40000') {
-																var gasUsed = new BigNumber(block.gasUsed);
-																var totalGasUsed = new BigNumber(gasUsed.times(block.transactions[block.transactions.length - 1].gasPrice));
-																var actionValue = new BigNumber(trace.action.value);
-																trace.action.value = web3.toHex(actionValue.plus(totalGasUsed).toNumber());
-															}
-															ineachCallback();
-														} else if (trace.type === 'call') {
-															if (tokenExporter[trace.action.to]) {
-																trace.isContract = true;
-																async.waterfall([
-																	function (tokeninfocallback) {
-																		tokenExporter[trace.action.to].db.find({
-																			$or: [{
-																				"args._from": trace.action.from
-																			}, {
-																				"args._to": trace.action.from
-																			}]
-																		}).sort({
-																			timestamp: -1
-																		}).skip(0).limit(10000).exec(function (err, events) {
-																			var tmpTokeninfo = {};
-																			tmpTokeninfo.account = trace.action.to;
-																			tmpTokeninfo.events = events;
-																			tmpTokeninfo.name = tokenExporter[trace.action.to].token_name;
-																			tmpTokeninfo.decimals = tokenExporter[trace.action.to].token_decimals;
-																			tmpTokeninfo.symbol = tokenExporter[trace.action.to].token_symbol;
-																			tmpTokeninfo.totalSupply = tokenExporter[trace.action.to].token_totalSupply;
-																			tokeninfocallback(err, tmpTokeninfo);
-																		});
-																	}
-																], function (err, tokeninfo) {
-																	if (err) {
-																		console.log("Error ", err);
-																		ineachCallback();
-																	} else {
-																		if (tokeninfo) {
-																			if (tokenExporter[trace.action.to].token_decimals) {
-																				trace.token_decimals = tokenExporter[trace.action.to].token_decimals;
-																			} else {
-																				trace.token_decimals = 0;
-																			}
+														ineachCallback();
+													} else if (trace.type === 'call') {
+														if (tokenExporter[trace.action.to]) {
+															trace.isContract = true;
+															async.waterfall([
+																function (tokeninfocallback) {
+																	tokenExporter[trace.action.to].db.find({
+																		$or: [{
+																			"args._from": trace.action.from
+																		}, {
+																			"args._to": trace.action.from
+																		}]
+																	}).sort({
+																		timestamp: -1
+																	}).skip(0).limit(10000).exec(function (err, events) {
+																		var tmpTokeninfo = {};
+																		tmpTokeninfo.account = trace.action.to;
+																		tmpTokeninfo.events = events;
+																		tmpTokeninfo.name = tokenExporter[trace.action.to].token_name;
+																		tmpTokeninfo.decimals = tokenExporter[trace.action.to].token_decimals;
+																		tmpTokeninfo.symbol = tokenExporter[trace.action.to].token_symbol;
+																		tmpTokeninfo.totalSupply = tokenExporter[trace.action.to].token_totalSupply;
+																		tokeninfocallback(err, tmpTokeninfo);
+																	});
+																}
+															], function (err, tokeninfo) {
+																if (err) {
+																	console.log("Error ", err);
+																	ineachCallback();
+																} else {
+																	if (tokeninfo) {
+																		if (tokenExporter[trace.action.to].token_decimals) {
+																			trace.token_decimals = tokenExporter[trace.action.to].token_decimals;
+																		} else {
+																			trace.token_decimals = 0;
+																		}
 
-																			if (tokenExporter[trace.action.to].token_symbol) {
-																				trace.token_symbol = tokenExporter[trace.action.to].token_symbol;
-																			} else {
-																				trace.token_symbol = 'n/a';
-																			}
+																		if (tokenExporter[trace.action.to].token_symbol) {
+																			trace.token_symbol = tokenExporter[trace.action.to].token_symbol;
+																		} else {
+																			trace.token_symbol = 'n/a';
+																		}
 
-																			if (tokeninfo && tokeninfo.events) {
-																				async.eachSeries(tokeninfo.events, function (event, eventeachCallback) {
-																					if (trace.transactionHash === event.transactionHash) {
-																						if (event.event === "Transfer" || event.event === "Approval") {
-																							if (event.args && event.args._value && trace.transactionPosition == event.transactionIndex) {
-																								trace.action._value = web3.toHex((new BigNumber(event.args._value)).toNumber());
-																								trace.action._to = event.args._to;
-																							}
-																						} else {
-																							console.dir(event.args);
+																		if (tokeninfo && tokeninfo.events) {
+																			async.eachSeries(tokeninfo.events, function (event, eventeachCallback) {
+																				if (trace.transactionHash === event.transactionHash) {
+																					if (event.event === "Transfer" || event.event === "Approval") {
+																						if (event.args && event.args._value && trace.transactionPosition == event.transactionIndex) {
+																							trace.action._value = web3.toHex((new BigNumber(event.args._value)).toNumber());
+																							trace.action._to = event.args._to;
 																						}
+																					} else {
+																						console.dir(event.args);
 																					}
-																					eventeachCallback();
-																				}, function (err) {
-																					ineachCallback();
-																				});
-																			} else {
+																				}
+																				eventeachCallback();
+																			}, function (err) {
 																				ineachCallback();
-																			}
+																			});
 																		} else {
 																			ineachCallback();
 																		}
+																	} else {
+																		ineachCallback();
 																	}
-																});
-															} else {
-																ineachCallback();
-															}
-														} else if (trace.type === 'create') {
-															trace.isContract = true;
-															//console.dir(trace);
-															//console.log("-----------------------trace.type === 'create'-----------------------");
-															if (tokenExporter[trace.result.address]) {
-																if (tokenExporter[trace.result.address].token_decimals) {
-																	trace.token_decimals = tokenExporter[trace.result.address].token_decimals;
-																} else {
-																	trace.token_decimals = 0;
 																}
-
-																if (tokenExporter[trace.result.address].token_symbol) {
-																	trace.token_symbol = tokenExporter[trace.result.address].token_symbol;
-																} else {
-																	trace.token_symbol = 'n/a';
-																}
-															}
+															});
+														} else {
 															ineachCallback();
 														}
-													}, function (err) {
-														incallback(err, traces);
-													});
-												},
-												function (traces, incallback) {
-													async.eachSeries(traces, function (trace, ineachCallback) {
-														if (trace.action.value) {
-															trace.action.value = web3.toHex((new BigNumber(trace.action.value)).toNumber());
-														}
-														if (trace.token_decimals) {
-															trace.token_decimals = web3.toHex((new BigNumber(trace.token_decimals)).toNumber());
-														}
-
-														if (trace.transactionHash) {
-															var rds_tx_value = {
-																transactionHash: trace.transactionHash,
-																blockNumber: trace.blockNumber,
-																date: block.timestamp ? block.timestamp.toString() : 0,
-																type: trace.type,
-																from: trace.action.from ? trace.action.from : "",
-																to: trace.action.to ? trace.action.to : "",
-																transactionPosition: trace.transactionPosition,
-																value: trace.action.value ? trace.action.value : 0,
-																isContract: trace.isContract,
-																token_symbol: trace.token_symbol ? trace.token_symbol : "",
-																token_decimals: trace.token_decimals ? trace.token_decimals : "",
-																_value: trace.action._value ? trace.action._value : "",
-																_to: trace.action._to ? trace.action._to : ""
-															};
-															redis.hmset(pre_fix_tx.concat(trace.transactionHash), rds_tx_value);
-
-															redis.zadd(pre_fix_tx.concat("list"), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
-															if (trace.action._to) {
-																redis.zadd(pre_fix_account_tx.concat(trace.action._to), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+													} else if (trace.type === 'create') {
+														trace.isContract = true;
+														//console.dir(trace);
+														//console.log("-----------------------trace.type === 'create'-----------------------");
+														if (tokenExporter[trace.result.address]) {
+															if (tokenExporter[trace.result.address].token_decimals) {
+																trace.token_decimals = tokenExporter[trace.result.address].token_decimals;
 															} else {
-																if (trace.action.to) {
-																	redis.zadd(pre_fix_account_tx.concat(trace.action.to), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
-																}
+																trace.token_decimals = 0;
 															}
-															if (trace.action.from) {
-																redis.zadd(pre_fix_account_tx.concat(trace.action.from), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+
+															if (tokenExporter[trace.result.address].token_symbol) {
+																trace.token_symbol = tokenExporter[trace.result.address].token_symbol;
+															} else {
+																trace.token_symbol = 'n/a';
 															}
-														} else if (trace.type === 'reward' && trace.blockHash) {
-															var rds_mining_value = {
-																blockHash: trace.blockHash,
-																blockNumber: trace.blockNumber,
-																date: block.timestamp ? block.timestamp.toString() : 0,
-																type: trace.type,
-																rewardType: trace.action.rewardType,
-																author: trace.action.author ? trace.action.author : "",
-																value: trace.action.value ? trace.action.value : 0,
-																isContract: trace.isContract,
-															};
-															redis.hmset(pre_fix_tx.concat(trace.blockHash), rds_mining_value);
-															if (trace.blockHash && trace.action.author) {
-																redis.zadd(pre_fix_account_tx.concat(trace.action.author), parseInt(block.timestamp), trace.blockHash);
-															}
-														} else {
-															console.log("[!trace.transactionHash]", trace);
 														}
 														ineachCallback();
-													}, function (err) {
-														incallback(err);
-													});
-												}
-											],
+													}
+												}, function (err) {
+													incallback(err, traces);
+												});
+											},
+											function (traces, incallback) {
+												async.eachSeries(traces, function (trace, ineachCallback) {
+													if (trace.action.value) {
+														trace.action.value = web3.toHex((new BigNumber(trace.action.value)).toNumber());
+													}
+													if (trace.token_decimals) {
+														trace.token_decimals = web3.toHex((new BigNumber(trace.token_decimals)).toNumber());
+													}
+
+													if (trace.transactionHash) {
+														var rds_tx_value = {
+															transactionHash: trace.transactionHash,
+															blockNumber: trace.blockNumber,
+															date: block.timestamp ? block.timestamp.toString() : 0,
+															type: trace.type,
+															from: trace.action.from ? trace.action.from : "",
+															to: trace.action.to ? trace.action.to : "",
+															transactionPosition: trace.transactionPosition,
+															value: trace.action.value ? trace.action.value : 0,
+															isContract: trace.isContract,
+															token_symbol: trace.token_symbol ? trace.token_symbol : "",
+															token_decimals: trace.token_decimals ? trace.token_decimals : "",
+															_value: trace.action._value ? trace.action._value : "",
+															_to: trace.action._to ? trace.action._to : ""
+														};
+														redis.hmset(pre_fix_tx.concat(trace.transactionHash), rds_tx_value);
+
+														redis.zadd(pre_fix_tx.concat("list"), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+														if (trace.action._to) {
+															redis.zadd(pre_fix_account_tx.concat(trace.action._to), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+														} else {
+															if (trace.action.to) {
+																redis.zadd(pre_fix_account_tx.concat(trace.action.to), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+															}
+														}
+														if (trace.action.from) {
+															redis.zadd(pre_fix_account_tx.concat(trace.action.from), parseInt(block.timestamp) + parseInt(trace.transactionPosition), trace.transactionHash);
+														}
+													} else if (trace.type === 'reward' && trace.blockHash) {
+														var rds_mining_value = {
+															blockHash: trace.blockHash,
+															blockNumber: trace.blockNumber,
+															date: block.timestamp ? block.timestamp.toString() : 0,
+															type: trace.type,
+															rewardType: trace.action.rewardType,
+															author: trace.action.author ? trace.action.author : "",
+															value: trace.action.value ? trace.action.value : 0,
+															isContract: trace.isContract,
+														};
+														redis.hmset(pre_fix_tx.concat(trace.blockHash), rds_mining_value);
+														if (trace.blockHash && trace.action.author) {
+															redis.zadd(pre_fix_account_tx.concat(trace.action.author), parseInt(block.timestamp), trace.blockHash);
+														}
+													} else {
+														console.log("[!trace.transactionHash]", trace);
+													}
+													ineachCallback();
+												}, function (err) {
+													incallback(err);
+												});
+											}
+										],
 											function (err) {
 												if (err) {
 													console.log("outeachCallback Error ", err);
